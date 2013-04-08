79b558200dd81f415f0cd35a42fa865196ecc94b
Kevin Wang <knwang@gmail.com>
2013-04-07 23:33:09 -0400

    Before object refactoring

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -21,7 +21,19 @@ class TodosController < AuthenticatedController
   def create
     @todo = Todo.new(params[:todo])
     if @todo.save_with_tags
-      EmailWorker.perform_async(current_user.id, @todo.id)
+      if current_user.created_at < Date.new(2010, 1, 1) || current_user.plan.premium?
+        new_credit_balance = current_user.current_credit_balance - 1
+      else
+        new_credit_balance = current_user.current_credit_balance - 2
+      end
+
+      if new_credit_balance < 0
+        AppMailer.notify_not_enough_balance(current_user).deliver
+      else
+        current_user.current_credit_balance = new_credit_balance
+        current_user.save
+        AppMailer.notify_low_balance(current_user).deliver if current_user.current_credit_balance < 10
+      end
       redirect_to root_path
     else
       render :new
